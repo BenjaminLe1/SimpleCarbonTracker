1c62ae4863cf3db96fbbdb3cfe006ec5ea727a52
BenjaminLe1 <le166@purdue.edu>
2023-07-04 14:46:34 -0500

    s

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +9,25 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* import React from "react";
+import { Nav, NavLink, NavMenu }
+    from "./NavbarElements";
+ 
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavMenu>
+                    <NavLink to="/about" activeStyle>
+                        About
+                    </NavLink>
+                    <NavLink to="/quiz" activeStyle>
+                        Contact Us
+                    </NavLink>
+                </NavMenu>
+            </Nav>
+        </>
+    );
+};
+ 
+export default Navbar; */
