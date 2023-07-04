4c27778240c0fcbe3aa680e45e6e6a191f09382c
BenjaminLe1 <le166@purdue.edu>
2023-07-04 16:11:35 -0500

    red

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,13 @@
 import React from "react";
  
+const customStyle = {
+    color: "red"
+};
+
 const About = () => {
     return (
         <div>
-            <h1>
+            <h1 style={customStyle}>
                 This is where you can check your footprint ! ! ! (carbon)
             </h1>
         </div>
