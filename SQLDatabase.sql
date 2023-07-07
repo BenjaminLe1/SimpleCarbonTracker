8effc35236930f30b9599f3c856d2ca44e7fd98e
BenjaminLe1 <le166@purdue.edu>
2023-07-06 20:03:00 -0500

    sad

--- SQLDatabase.sql
+++ SQLDatabase.sql
@@ -1,1 +1,12 @@
+CREATE TABLE `Person` (
+  `idPerson` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`idPerson`),
+  UNIQUE KEY `idPerson_UNIQUE` (`idPerson`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
 SELECT * FROM SimpleCarbonTracker.Person;
