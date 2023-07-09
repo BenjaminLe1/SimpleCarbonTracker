88ea23a8eaef0e794e5856439ccfcf185805c5d3
BenjaminLe1 <le166@purdue.edu>
2023-07-08 19:37:06 -0500

    temp

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,9 @@ app.use(cors({
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"N@vi03kid",
+    //password:"N@vi03kid", //vijay
+    //password:"Canbe56&8", //bens
+    //password:"dataBaseNow12" //pravin
     database:"simplecarbontracker"
 })
 
