a7c2cbef131bdfaf7b734af2ebfb1153935cebbb
BenjaminLe1 <le166@purdue.edu>
2023-07-08 20:53:25 -0500

    vij nip

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -10,6 +10,7 @@ const Home = () => {
         <div>
             <h1 style={customStyle}>
                 This is where you can check your footprint ! ! ! stfu rhino(carbon)
+                <img src="./VJ.jpeg" alt="" />
             </h1>
         </div>
     );
