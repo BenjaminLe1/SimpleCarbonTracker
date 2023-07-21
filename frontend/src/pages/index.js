f0acdfbe52767a2e451955b2a4e5ede853f81642
Vijay Kapistalam <vijaykapistalam@Vijays-MBP.attlocal.net>
2023-07-20 22:12:11 -0500

    more css

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './pages.css'
 
 const customStyle = {
     color: "red"
@@ -10,7 +10,7 @@ const Home = () => {
 
     return (
         <div>
-            <h1 style={customStyle}>
+            <h1>
                 HOMEPAGE
             </h1>
         </div>
