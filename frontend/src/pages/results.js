99e01764f83dfef33ae0eb3d40baccd0ca59c551
BenjaminLe1 <le166@purdue.edu>
2023-07-22 11:40:53 -0500

    results page finished

--- frontend/src/pages/results.js
+++ frontend/src/pages/results.js
@@ -9,12 +9,22 @@ function Results(){
     const[cat4,setCat4] = useState(0)
     const[overall, setOverall] = useState(0)
 
-    function getScores(){
+    const getScores=async()=>{
+        //NEED TO REWORK PERSON TABLE TO HAVE JUST (4 category score columns) CATEGORY SCORE (instead of idQA)
         setCat1(12)
         setCat2(15)
         setCat3(6)
         setCat4(24)
         setOverall((cat1+cat2+cat3+cat4)/4)
+
+        //actual code
+        //send idPerson if needed
+        /* const response = await axios.get("http://localhost:4000/get_scores");
+        setCat1(response.data[0].Category1_Score);
+        setCat2(response.data[1].Category2_Score);
+        setCat3(response.data[2].Category3_Score);
+        setCat4(response.data[3].Category4_Score);
+        setOverall((cat1+cat2+cat3+cat4)/4) */
     }
     function getPerc(score){
         return (score / 30) * 100
