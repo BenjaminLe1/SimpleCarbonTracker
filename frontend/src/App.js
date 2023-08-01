14b92482ecab8398cc38a11ecb80f8296c173d3d
BenjaminLe1 <le166@purdue.edu>
2023-07-31 22:27:21 -0500

    extra

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Routes, Route }
     from 'react-router-dom';
 import Home from './pages';
 import Quiz from './pages/quiz';
-import Registration from './pages/registration';
 
 import Login from './pages/login'; 
 import Signup from './pages/signup';
@@ -37,14 +36,11 @@ function App() {
             <Routes>
                 <Route path='/' element={<Home/>} />
                 <Route path='/quiz' element={<Quiz/>}/>
-                <Route path='/account' element={<Registration/>}/>
 
                 <Route path='/login' element={<Login />}/>
                 <Route path='/signup' element={<Signup/>}/>
-
-
-
                 <Route path='/signout' element={<Signout />} />
+                
                 <Route path='/results' element={<Result />} />
                 <Route path='/youareamonkey' element={<Monkey />} />
                 <Route path='/youareanangel' element={<Angel />} />
