2718af9f9b72c9640b58abf65081a20d63334c18
BenjaminLe1 <le166@purdue.edu>
2023-07-31 21:41:59 -0500

    HomePage

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './pages.css'
+import './home.css'
 //import axios from 'axios';
 //import { useState,useEffect } from 'react';
 
@@ -9,14 +9,40 @@ const Home = () => {
         axios.get("http://localhost:4000/check_login")
     ], []) */
     //homepage refresh
+    //<a href="http://localhost:3000/quiz">Take the first step!</a>
     return (
         <div>
-            <div>
-                <h1>
-                </h1>
+            <h1 className="xh1">
+                Do you want these monkeys die?
+            </h1>
+            <h3 className="xh3">
+                Take a step towards environmental awareness and find out how much impact your daily choices have on the planet. Our interactive quiz will assess your carbon footprint by evaluating various aspects of your lifestyle, such as transportation, dietary and spending habits, and home energy consumption. Uncover your carbon footprint score and receive personalized tips on how to reduce your environmental impact. Start the quiz now and be part of the solution!
+            </h3>
+            <div className="buttons-container">
+                <a href="http://localhost:3000/quiz" class="button button--pen">
+                    <div class="button__wrapper">
+                        <span class="button__text">Click Here</span>
+                    </div>
+                    <div class="characterBox">
+                        <div class="character wakeup">
+                            <div class="character__face"></div>
+                            <div class="charactor__face2"></div>
+                        </div>
+                        <div class="character wakeup">
+                            <div class="character__face"></div>
+                            <div class="charactor__face2"></div>
+                        </div>
+                        <div class="character">
+                            <div class="character__face"></div>
+                            <div class="charactor__face2"></div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div className="picContainer">
+                <img src={require("./images/Pravin.png")} className="homePic" alt="homePic"></img>
             </div>
         </div>
     );
 };
 export default Home;
-
