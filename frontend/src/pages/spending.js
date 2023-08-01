e582f1bbe1b751a17999001274a7048843a951ee
Vijay Kapistalam <vijaykapistalam@Vijays-MacBook-Pro.local>
2023-07-31 22:49:14 -0500

    results page done

--- frontend/src/pages/spending.js
+++ frontend/src/pages/spending.js
@@ -3,8 +3,31 @@ import "./pages.css"
 const Spending = () => {
     return (
         <div>
-            <b>Header</b>
-            <p>Spending info</p>
+            <div className="house1">
+            <h2 className="houseTitle1">Be Mindful of Appliance Spending</h2>
+            <div className="description">
+                <p>Being mindful of our appliance spending is crucial for reducing our environmental impact and promoting a more sustainable lifestyle. Before acquiring new appliances, carefully evaluate your actual needs, as existing appliances can often be repaired or repurposed effectively, saving both money and resources. When making purchases, prioritize energy-efficient models to decrease long-term utility bills and lower your overall carbon footprint. Additionally, researching and comparing prices, features, and energy ratings will help you find appliances that align with your requirements while being more eco-friendly. Consider exploring second-hand or refurbished options as a budget-friendly and sustainable alternative. Many pre-owned appliances are in good condition and can serve their purpose efficiently. Regularly maintaining your appliances by cleaning them and scheduling timely repairs prolongs their lifespan and reduces the need for frequent replacements. Embracing conscious appliance spending contributes to reducing waste and conserving valuable resources. Thank you for considering these recommendations and taking steps toward a more responsible and environmentally friendly approach to purchasing appliances.
+
+</p>
+</div>
+            </div>
+
+            
+            <div className="house1">
+            <h2 className="houseTitle1">Buy Second Hand or Eco-Friendly Clothing</h2>
+            <div className="description">
+                <p>Buying second-hand clothing is an excellent way to embrace sustainability in fashion. Second-hand shopping extends the life of garments, reducing textile waste and minimizing the demand for new clothing production. We encourage you to explore thrift stores, consignment shops, and online platforms that offer a wide range of stylish and affordable second-hand options. When looking for new pieces to add to your wardrobe, consider supporting sustainable fashion brands that use renewable or eco-friendly materials. Fabrics like organic cotton, hemp, Tencel, or recycled materials have a lower environmental impact, and choosing garments made from these materials supports a more sustainable fashion industry. You might also enjoy participating in clothing swapping events or platforms, where you can exchange clothes with others. These initiatives promote a circular economy and contribute to reducing clothing waste. Repairing or upcycling worn-out clothing is another creative way to extend their life. Simple alterations or DIY projects can breathe new life into old pieces and give them a fresh and stylish twist. By embracing second-hand clothing and garments made with renewable materials, you're contributing to a more sustainable fashion industry and reducing your ecological footprint.
+</p>
+</div>  
+            </div>
+
+            <div className="house1">
+            <h2 className="houseTitle1">Reduce Luxery Spending</h2>
+            <div className="description">
+                <p>One of the most significant contributors to our carbon footprint is the production and transportation of luxury goods. From fashion items to high-end electronics, the manufacturing process and global supply chains involved in producing luxuries often result in higher greenhouse gas emissions. By reducing luxury spending, you can actively lower your carbon footprint and contribute to a more sustainable future. Instead of investing in excessive material possessions, consider prioritizing experiences and activities that have a lower environmental impact. Engaging in nature-based activities, exploring local cultural events, or spending time with loved ones can provide meaningful and fulfilling experiences without the environmental burden. Furthermore, allocating a specific budget for luxuries allows you to make conscious and well-informed choices. Consider investing in eco-friendly or ethically sourced luxury items and experiences, supporting brands and businesses that prioritize sustainability. Choosing products made from renewable or recycled materials, or opting for ethically manufactured goods, aligns your indulgences with responsible consumerism. By making mindful decisions about luxury spending and considering the environmental implications of our choices, we can collectively make a difference in combating climate change.
+</p>
+</div>  
+            </div>
         </div>
     );
 }
--- frontend/src/pages/transportation.js
+++ frontend/src/pages/transportation.js
@@ -3,8 +3,30 @@ import "./pages.css"
 const Transportation = () => {
     return (
         <div>
-            <b>Header</b>
-            <p>Transportation info</p>
+            <div className="house1">
+            <h2 className="houseTitle1">Walk or Bike Instead of Driving</h2>
+            <div className="description">
+                <p> Motorized transportation, such as cars or public transportation, contributes to greenhouse gas emissions and air pollution, which can harm our planet's health. As you continue tracking your carbon footprint, we recommend considering more eco-friendly alternatives like walking and biking. They produce zero emissions, helping to reduce your carbon footprint and promote cleaner air. Moreover, incorporating more walking and biking into your routine can improve your personal health and well-being. To start, think about opportunities to walk or bike for short distances, such as running errands or commuting to nearby places. You might discover that walking or biking to nearby destinations like the grocery store or park is not only environmentally friendly but also a refreshing and enjoyable experience. Be sure to check for available bike lanes, walking trails, or pedestrian-friendly routes in your area to make your active transportation choices safer and more convenient. Additionally, consider biking to work on sunny days or walking to nearby places when the weather permits. By choosing walking or biking over motorized transportation for some of your trips, you're actively contributing to a cleaner and greener future.
+</p>
+</div>
+            </div>
+
+            
+            <div className="house1">
+            <h2 className="houseTitle1">Explore More Eco-Friendly Transportation</h2>
+            <div className="description">
+                <p>If you're looking to reduce your environmental impact, consider exploring more eco-friendly options for your daily commute. If you're driving a conventional gasoline or diesel-powered car, transitioning to an electric or hybrid vehicle could be a game-changer. Electric cars produce zero tailpipe emissions and can be charged with renewable energy sources, making them an excellent choice for reducing air pollution and greenhouse gas emissions. Hybrid vehicles, on the other hand, offer a combination of an internal combustion engine and an electric motor, providing better fuel efficiency and lower emissions compared to traditional vehicles. These options significantly reduce your carbon footprint and contribute to a more sustainable transportation system. 
+</p>
+</div>  
+            </div>
+
+            <div className="house1">
+            <h2 className="houseTitle1">Consider Alternatives to Flying for Certain Trips</h2>
+            <div className="description">
+                <p>Flying has a significant environmental impact, as it contributes to greenhouse gas emissions and climate change. To reduce your carbon footprint and promote sustainable travel, we recommend considering alternatives to flying for certain trips. One effective approach is to explore alternative transportation options for shorter distances. Opt for train or bus travel when feasible, as these modes of transportation generally have lower emissions compared to flying. By choosing greener options for regional travel, you can significantly reduce your carbon footprint. By being mindful of your flying habits and making conscious choices to fly less, you're taking positive steps toward a more sustainable world. 
+</p>
+</div>  
+            </div>
         </div>
     );
 }
